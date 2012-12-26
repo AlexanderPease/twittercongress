f3296dd56e9cd6e224a143dc73fb3141af0f2fff
AlexanderPease <alexanderpease@usv.com>
2012-12-25 22:31:17 -0500

    Started figuring out results.html and corresponding def results in views.py

--- congress_app/urls.py
+++ congress_app/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('congress_app.views',
     url(r'^$', 'index'),
-   # url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^results','results'),
+    # url(r'^(?P<poll_id>\d+)/$', 'detail'),
    # url(r'^(?P<poll_id>\d+)/results/$', 'results'),
    # url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
--- congress_app/views.py
+++ congress_app/views.py
@@ -1,7 +1,20 @@
 # Views for congress_app
 from django.shortcuts import get_object_or_404, render_to_response
+from sunlight import congress
+
+SUNLIGHT_API_KEY = '6beac436fa02439abfe8f27909ab3d8f'
 
 def index(request):
+    foo = congress.legislators(lastname='Pelosi')[0]
     return render_to_response('congress_app/index.html',)
+
+def results(request):#, zip_code):
+    #district = congress.districts.getDistrictsFromZip('01741')
+    #state = district['state']
+    
+    foo = {'town': 'Greenland, NH', 'phone_number': '603-479-0310'}
+    return render_to_response('congress_app/results.html', {'results': foo})
+
+
 
 
