973602d0f7e9beda8445506f01ca727d7a7c0663
AlexanderPease <alexanderpease@usv.com>
2012-12-25 20:26:45 -0500

    Now running on /congress_app/

--- congress_app/views.py
+++ congress_app/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+# Views for congress_app
+from django.shortcuts import get_object_or_404, render_to_response
+
+def index(request):
+    return render_to_response('congress_app/index.html',)
+
+
--- twittercongress/urls.py
+++ twittercongress/urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
+    url(r'^congress_app/', include('congress_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
