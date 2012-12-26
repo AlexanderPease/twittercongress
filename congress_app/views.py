09b8cfea00412004c94298010f0d92c79fee3f58
AlexanderPease <alexanderpease@usv.com>
2012-12-26 11:14:09 -0500

    Working pprint debug statement, Sunlight API is working

--- congress_app/views.py
+++ congress_app/views.py
@@ -1,8 +1,7 @@
 # Views for congress_app
 from django.shortcuts import get_object_or_404, render_to_response
 from sunlight import congress
-
-SUNLIGHT_API_KEY = '6beac436fa02439abfe8f27909ab3d8f'
+from pprint import pprint
 
 def index(request):
     foo = congress.legislators(lastname='Pelosi')[0]
@@ -11,8 +10,9 @@ def index(request):
 def results(request):#, zip_code):
     #district = congress.districts.getDistrictsFromZip('01741')
     #senators = congress.legislators.getList(state=district.state, district = district.number)
-    
-    foo = {'town': 'Greenland', 'phone_number': '603-479-0310', 'twitter_id': 'twitter'}
+    senators = congress.legislators(lastname='Pelosi')
+    pprint(senators)
+    foo = {'town': 'f', 'phone_number': '603-479-0310', 'twitter_id': 'twitter'}
     return render_to_response('congress_app/results.html', {'results': foo})
 
 
