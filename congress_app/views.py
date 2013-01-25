548c8bb0feb91cfc7de36d01a5fbcb65fdbb5204
AlexanderPease <alexanderpease@usv.com>
2013-01-25 15:39:30 -0500

    Cleaned up views.py, moved static states var into constants.py

--- congress_app/views.py
+++ congress_app/views.py
@@ -6,66 +6,8 @@ import os.path
 from sunlight import congress
 from pprint import pprint
 import re #regular expressions
+import constants
 
-states = {
-        'AK': 'Alaska',
-        'AL': 'Alabama',
-        'AR': 'Arkansas',
-        'AS': 'American Samoa',
-        'AZ': 'Arizona',
-        'CA': 'California',
-        'CO': 'Colorado',
-        'CT': 'Connecticut',
-        'DC': 'District of Columbia',
-        'DE': 'Delaware',
-        'FL': 'Florida',
-        'GA': 'Georgia',
-        'GU': 'Guam',
-        'HI': 'Hawaii',
-        'IA': 'Iowa',
-        'ID': 'Idaho',
-        'IL': 'Illinois',
-        'IN': 'Indiana',
-        'KS': 'Kansas',
-        'KY': 'Kentucky',
-        'LA': 'Louisiana',
-        'MA': 'Massachusetts',
-        'MD': 'Maryland',
-        'ME': 'Maine',
-        'MI': 'Michigan',
-        'MN': 'Minnesota',
-        'MO': 'Missouri',
-        'MP': 'Northern Mariana Islands',
-        'MS': 'Mississippi',
-        'MT': 'Montana',
-        'NA': 'National',
-        'NC': 'North Carolina',
-        'ND': 'North Dakota',
-        'NE': 'Nebraska',
-        'NH': 'New Hampshire',
-        'NJ': 'New Jersey',
-        'NM': 'New Mexico',
-        'NV': 'Nevada',
-        'NY': 'New York',
-        'OH': 'Ohio',
-        'OK': 'Oklahoma',
-        'OR': 'Oregon',
-        'PA': 'Pennsylvania',
-        'PR': 'Puerto Rico',
-        'RI': 'Rhode Island',
-        'SC': 'South Carolina',
-        'SD': 'South Dakota',
-        'TN': 'Tennessee',
-        'TX': 'Texas',
-        'UT': 'Utah',
-        'VA': 'Virginia',
-        'VI': 'Virgin Islands',
-        'VT': 'Vermont',
-        'WA': 'Washington',
-        'WI': 'Wisconsin',
-        'WV': 'West Virginia',
-        'WY': 'Wyoming'
-}
 
 def index(request):
     zip_code = request.GET.get('zip_code')
@@ -84,7 +26,7 @@ def index(request):
     
     # Test for single congressional district
     if len(districts) > 1:
-        pass #debug
+        pprint(str(len(districts)) + ' DISTRICTS IN ZIP CODE') #debug
     
     #Pull out three representatives
     district = districts[0]
@@ -103,6 +45,6 @@ def index(request):
              legislator['image'] = 'img/200x250/' + 'DEFAULT.jpg'
 
     # Add in full state name and district
-    legislators['state'] = states.get(district['state'])
+    legislators['state'] = constants.states.get(district['state'])
 
     return render_to_response('results.html', {'results': legislators}, context_instance=RequestContext(request))
