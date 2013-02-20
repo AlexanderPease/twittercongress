b836492fc003c28078c6628279144ee80ce07bbd
AlexanderPease <alexanderpease@usv.com>
2013-02-20 11:39:26 -0500

    Right before Heroku deploy

--- congress_app/views.py
+++ congress_app/views.py
@@ -4,32 +4,60 @@ from django.template import Context, RequestContext, loader
 from django.conf import settings #for STATIC_URL
 import os.path
 from sunlight import congress
+from geopy import geocoders
 from pprint import pprint
 import re #regular expressions
 import constants
 
+#def district_for_address(address, request):
+#    return districts[0]
+
+#def district_for_zip_code(zip_code, request):
+#    return districts[0]
+
 
 def index(request):
     zip_code = request.GET.get('zip_code')
+    address = request.GET.get('address')
+
+    # Address takes precedence over ZIP code for finding legislators
+    # User is only asked for address if ZIP code contains multiple districts
+    if address:
+        #district = district_for_address(address, request)    
+        g = geocoders.Google()
+        try:
+            place, (lat, lon) = g.geocode(address)
+            pprint(place) #support multiple locations
+        except: 
+             return render_to_response('index.html', {'error_bad_address': address}, context_instance=RequestContext(request))
+        districts = congress.districts_for_lat_lon(lat, lon)
+        if len(districts) != 1: 
+            pprint('Multiple districts for single geopoint?!') #debug
+        else:
+            district = districts[0]
+
+    # ZIP code method
+    else:
+        #district = district_for_zip_code(zip_code, request) 
+            # Is there a zip_code arg? If not, return home page
+        if not zip_code:
+            return render_to_response('index.html', context_instance=RequestContext(request))
+
+        districts = congress.districts_for_zip(zip_code)
+        # Test if zip_code is valid
+        regex = re.compile('\d{5,5}')
+        if not (regex.match(zip_code) and districts):
+            return render_to_response('index.html', {'error_zip_code': zip_code}, context_instance=RequestContext(request))
+        
+        # Test for single congressional district
+        if len(districts) > 1:
+            pprint(str(len(districts)) + ' DISTRICTS IN ZIP CODE') #debug
+            return render_to_response('index.html', {'error_multiple_districts': zip_code}, context_instance=RequestContext(request))
+        else: 
+            district = districts[0]
+        
     
-    # Is there a zip_code?
-    if not zip_code:
-        return render_to_response('index.html', context_instance=RequestContext(request))
-   
-   # Get data from Sunlight module
-    districts = congress.districts_for_zip(zip_code)
-    
-    # Test if zip_code is valid
-    regex = re.compile('\d{5,5}')
-    if not (regex.match(zip_code) and districts):
-         return render_to_response('index.html', {'error_message': zip_code}, context_instance=RequestContext(request))
-    
-    # Test for single congressional district
-    if len(districts) > 1:
-        pprint(str(len(districts)) + ' DISTRICTS IN ZIP CODE') #debug
-    
-    #Pull out three representatives
-    district = districts[0]
+    # Get representatives from district
     representative = congress.legislators(title='Rep', state=district['state'], district=district['number'])[0]
     senators = congress.legislators(title='Sen', state=district['state'])
     legislators = {'representative': representative, 'senator1':senators[0], 'senator2':senators[1]}
@@ -45,6 +73,6 @@ def index(request):
              legislator['image'] = 'img/200x250/' + 'DEFAULT.jpg'
 
     # Add in full state name and district
-    legislators['state'] = constants.states.get(district['state'])
+    legislators['state'] = constants.states.get(legislators['representative']['state'])
 
     return render_to_response('results.html', {'results': legislators}, context_instance=RequestContext(request))
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
 Twitter Congress is a simple python program I made to create Tweet @ Congress, a website to easily find and follow your Congressional Representatives on Twitter. The idea came from two concepts:
 
-Representative democracy is best served by a continuous, unfiltered dialogue between citizens and their representatives.
-
-Social networks such as Twitter are perfectly made to faciliate this discussion on a daily basis.
+#Representative democracy is best served by a continuous, unfiltered dialogue between citizens and their representatives.
+#Social networks such as Twitter are perfectly made to faciliate this discussion on a daily basis.
