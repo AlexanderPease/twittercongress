4275a82f7c1a0b4b539e31ae6c6a236fa0116e11
AlexanderPease <alexanderpease@usv.com>
2013-04-15 12:35:01 -0400

    Changed requirements.txt and Procfile. About to try deploying to heroku

--- twittercongress/settings.py
+++ twittercongress/settings.py
@@ -135,7 +135,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django.contrib.humanize',
-    'polls',
+    #'polls',
     'congress_app',
 )
 
