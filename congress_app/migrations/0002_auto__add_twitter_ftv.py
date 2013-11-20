e6ad67c82cce8d4ba62a4d4e05b9e240be874a48
AlexanderPease <alexander@usv.com>
2013-11-20 14:12:59 -0500

    Reinitialized south, everything should be working

--- congress_app/migrations/0002_auto__add_twitter_ftv.py
+++ congress_app/migrations/0002_auto__add_twitter_ftv.py
@@ -8,35 +8,19 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'Politician'
-        db.create_table(u'congress_app_politician', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('state', self.gf('django.db.models.fields.CharField')(max_length=2)),
-            ('district', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=20)),
-            ('party', self.gf('django.db.models.fields.CharField')(max_length=1)),
-            ('portrait_id', self.gf('django.db.models.fields.CharField')(max_length=200, null=True, blank=True)),
-        ))
-        db.send_create_signal(u'congress_app', ['Politician'])
-
-        # Adding model 'Twitter'
-        db.create_table(u'congress_app_twitter', (
+        # Adding model 'Twitter_FTV'
+        db.create_table(u'congress_app_twitter_ftv', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('user_id', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
             ('handle', self.gf('django.db.models.fields.CharField')(max_length=15, null=True, blank=True)),
             ('politician', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['congress_app.Politician'], unique=True)),
         ))
-        db.send_create_signal(u'congress_app', ['Twitter'])
+        db.send_create_signal(u'congress_app', ['Twitter_FTV'])
 
 
     def backwards(self, orm):
-        # Deleting model 'Politician'
-        db.delete_table(u'congress_app_politician')
-
-        # Deleting model 'Twitter'
-        db.delete_table(u'congress_app_twitter')
+        # Deleting model 'Twitter_FTV'
+        db.delete_table(u'congress_app_twitter_ftv')
 
 
     models = {
@@ -53,6 +37,13 @@ class Migration(SchemaMigration):
         },
         u'congress_app.twitter': {
             'Meta': {'object_name': 'Twitter'},
+            'handle': ('django.db.models.fields.CharField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'politician': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['congress_app.Politician']", 'unique': 'True'}),
+            'user_id': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'congress_app.twitter_ftv': {
+            'Meta': {'object_name': 'Twitter_FTV'},
             'handle': ('django.db.models.fields.CharField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'politician': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['congress_app.Politician']", 'unique': 'True'}),
--- congress_app/models.py
+++ congress_app/models.py
@@ -169,3 +169,4 @@ class Twitter_FTV(models.Model):
 
 
 
+
